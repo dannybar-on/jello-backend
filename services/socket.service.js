446bf1b2d8b703d8cf8a7181a1ce5f6c1004ab74
Elad Ayal <eladdevmail@gmail.com>
2022-01-29 17:10:33 +0200

    Socket service up and running

--- services/socket.service.js
+++ services/socket.service.js
@@ -14,20 +14,36 @@ function connectSockets(http, session) {
         socket.on('disconnect', socket => {
             console.log('Someone disconnected')
         })
-        socket.on('chat topic', topic => {
-            if (socket.myTopic === topic) return;
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
+        // socket.on('chat topic', topic => {
+        //     if (socket.myTopic === topic) return;
+        //     if (socket.myTopic) {
+        //         socket.leave(socket.myTopic)
+        //     }
+        //     socket.join(topic)
+        //     socket.myTopic = topic
+        // })
+        // socket.on('chat newMsg', msg => {
+        //     console.log('Emitting Chat msg', msg);
+        //     // emits to all sockets:
+        //     // gIo.emit('chat addMsg', msg)
+        //     // emits only to sockets in the same room
+        //     gIo.to(socket.myTopic).emit('chat addMsg', msg)
+        // })
+        socket.on('member-joined', (boardId) => {
+            if (socket.boardId === boardId) return
+            if (socket.boardId) {
+                socket.leave(socket.boardId)
             }
-            socket.join(topic)
-            socket.myTopic = topic
+            socket.join(boardId)
+            socket.boardId = boardId
+            console.log('Member Joined to: ', boardId)
         })
-        socket.on('chat newMsg', msg => {
-            console.log('Emitting Chat msg', msg);
-            // emits to all sockets:
-            // gIo.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat addMsg', msg)
+
+        socket.on('board-update', updatedBoard => {
+            socket.to(socket.boardId).emit('board-update', updatedBoard )
+        })
+        socket.on('boards-update', boards => {
+            socket.broadcast.emit('boards-update', boards )
         })
         socket.on('user-watch', userId => {
             socket.join('watching:' + userId)
@@ -36,9 +52,9 @@ function connectSockets(http, session) {
             logger.debug(`Setting (${socket.id}) socket.userId = ${userId}`)
             socket.userId = userId
         })
-        socket.on('unset-user-socket', () => {
-            delete socket.userId
-        })
+        // socket.on('unset-user-socket', () => {
+        //     delete socket.userId
+        // })
 
     })
 }
